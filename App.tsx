b858f56e01ecf7a44904063c2f6ecc073e4612d8
Joel Beach <jtbeach@users.noreply.github.com>
2020-05-21 23:13:36 +1000

    Remove export

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Linking } from 'expo';
 
-export const useDeepLink = () => {
+const useDeepLink = () => {
   const [currentURL, setCurrentURL] = useState<string | null>(null);
   const [consumedInitialUrl, setConsumedInitialUrl] = useState(false);
 
